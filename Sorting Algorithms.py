f82a2ef75ed86182fe5126f6978b4ce8dd9f080c
Christopher Abboud <Chris.abboud1@gmail.com>
2020-10-01 11:42:49 -0400

    Eddited programs and fixed up Main

--- Sorting Algorithms.py
+++ Sorting Algorithms.py
@@ -1,34 +1,24 @@
-import random
 import time
 from Algorithms import *
 from UI_Functions import *
 
-List = []
-
-ListLength = float(input("How many numbers do you want in your list?: "))
-ListLowerRange = float(input("What is the lower range of the random list?: "))
-ListUpperRange = float(input("What is the upper range of the random list?: "))
-
-for i in range(int(ListLength)):
-    List.append(random.randint(int(ListLowerRange),int(ListUpperRange)))
-
+ListInputs = ListCreationInputs()
+List = GenerateList(ListInputs[0], ListInputs[1], ListInputs[2])
 print("The random list is: ", List)
 
 
-printMenu()
-
-choice = int(float(input("What sorting algorithm would you like to use?", )))
+Choice = printMenu()
 
 
+print("--------------------------------------------------------------")
 time1 = time.time()
 SelectionSort(List)
 time2 = time.time()
-
-print(List, "Time: ", time2 - time1)
+print("Selection Sort Time: ", time2 - time1)
 
 
 time1 = time.time()
 BubbleSort(List)
 time2 = time.time()
-print(List, "Time: ", time2 - time1)
+print("Bubble Sort Time: ", time2 - time1)
 
--- UI_Functions.py
+++ UI_Functions.py
@@ -1,3 +1,11 @@
+import random
+
+def ListCreationInputs():
+    ListLength = float(input("How many numbers do you want in your list?: "))
+    ListLowerRange = float(input("What is the lower range of the random list?: "))
+    ListUpperRange = float(input("What is the upper range of the random list?: "))
+    return [ListLength, ListLowerRange, ListUpperRange]
+    
 def printMenu():
     print()
     print("1. Selection Sort")
@@ -6,3 +14,11 @@ def printMenu():
     print("4. Merge Sort")
     print("5. Quick Sort")
     print()
+
+    return int(float(input("What sorting algorithm would you like to use? ", )))
+
+def GenerateList(ListLength, ListLowerRange, ListUpperRange):
+    List = []
+    for i in range(int(ListLength)):
+        List.append(random.randint(int(ListLowerRange),int(ListUpperRange)))
+    return List
